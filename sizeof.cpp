0368bed089be0dbad31ce19b3ec48b5288aa44aa
amruth0098 <86010025+amruth0098@users.noreply.github.com>
2022-01-03 13:12:35 +0530

    Add files via upload

---
+++ sizeof.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    float f;
+    char c;
+    double d;
+    
+    cout<<"the sizeof int is "<<sizeof(int)<<" bytes"<<endl;
+cout<<"the sizeof int is "<<sizeof(float)<<" bytes"<<endl;
+cout<<"the sizeof int is "<<sizeof(char)<<" bytes"<<endl;
+cout<<"the sizeof int is "<<sizeof(double)<<" bytes";
+    return 0;
+}
